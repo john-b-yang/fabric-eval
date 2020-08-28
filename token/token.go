910eac4a2f8e6b84b8b826afa378d221473feaab
John Yang <john.yang20@berkeley.edu>
2020-08-28 00:27:28 -0700

    [Token] Finished

--- token/token.go
+++ token/token.go
@@ -8,6 +8,8 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
+const tokenKey = "TOKEN-KEY"
+
 // Token struct for storing token information
 type Token struct {
 	Allowances map[cid.ClientIdentity](map[cid.ClientIdentity]int) `json:"allowances"`
@@ -38,9 +40,158 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface, 
 	if err != nil {
 		return fmt.Errorf("Error converting token to bytes: %s", err.Error())
 	}
-	err = ctx.GetStub().PutState(token.Symbol, tokenBytes)
+	err = ctx.GetStub().PutState(tokenKey, tokenBytes)
 	if err != nil {
 		return fmt.Errorf("Error writing token bytes to state: %s", err.Error())
+	}
+	return nil
+}
+
+// getToken is a helper function to retrieve the Token data
+func (s *SmartContract) getToken(ctx contractapi.TransactionContextInterface) (*Token, error) {
+	tokenBytes, err := ctx.GetStub().GetState(tokenKey)
+	if err != nil {
+		return nil, fmt.Errorf("Error retrieving Token from state: %s", err)
+	}
+	if tokenBytes == nil {
+		return nil, fmt.Errorf("Error, Token has not been created")
+	}
+
+	// Unmarshal game data bytes into game data struct
+	token := new(Token)
+	err = json.Unmarshal(tokenBytes, token)
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshaling proposal bytes: %s", err)
+	}
+
+	return token, nil
+}
+
+// Transfer allows a sender to transfer tokens to a receiver
+func (s *SmartContract) Transfer(ctx contractapi.TransactionContextInterface, receiver cid.ClientIdentity, numTokens int) error {
+	token, err := s.getToken(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	if numTokens > token.Balances[sender] {
+		return fmt.Errorf("Sender does not have enough balance to transfer desired amount")
+	}
+	token.Balances[sender] = token.Balances[sender] - numTokens
+	token.Balances[receiver] = token.Balances[receiver] + numTokens
+	tokenBytes, err := json.Marshal(token)
+	if err != nil {
+		return fmt.Errorf("Error converting Token to bytes: %s", err.Error())
+	}
+	err = ctx.GetStub().PutState(tokenKey, tokenBytes)
+	if err != nil {
+		return fmt.Errorf("Error writing Token bytes to state: %s", err.Error())
+	}
+	return nil
+}
+
+// Approve allows a sender to identify a delegate who can spend funds on their behalf
+func (s *SmartContract) Approve(ctx contractapi.TransactionContextInterface, delegate cid.ClientIdentity, numTokens int) error {
+	token, err := s.getToken(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	if numTokens > token.Balances[sender] {
+		return fmt.Errorf("Sender does not have enough balance to transfer desired amount")
+	}
+	if _, ok := token.Allowances[sender][delegate]; ok {
+		return fmt.Errorf("Allowance for specified delegate from sender has already been created")
+	}
+	token.Balances[sender] = token.Balances[sender] - numTokens
+	token.Allowances[sender][delegate] = numTokens
+	tokenBytes, err := json.Marshal(token)
+	if err != nil {
+		return fmt.Errorf("Error converting Token to bytes: %s", err.Error())
+	}
+	err = ctx.GetStub().PutState(tokenKey, tokenBytes)
+	if err != nil {
+		return fmt.Errorf("Error writing Token bytes to state: %s", err.Error())
+	}
+	return nil
+}
+
+// IncreaseAllowance allows a sender to increase the funds of a delegate
+func (s *SmartContract) IncreaseAllowance(ctx contractapi.TransactionContextInterface, delegate cid.ClientIdentity, numTokens int) error {
+	token, err := s.getToken(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	if numTokens > token.Balances[sender] {
+		return fmt.Errorf("Sender does not have enough balance to transfer desired amount")
+	}
+	if _, ok := token.Allowances[sender][delegate]; !ok {
+		return fmt.Errorf("Allowance for specified delegate from sender has not been created")
+	}
+	token.Balances[sender] = token.Balances[sender] - numTokens
+	token.Allowances[sender][delegate] = token.Allowances[sender][delegate] + numTokens
+	tokenBytes, err := json.Marshal(token)
+	if err != nil {
+		return fmt.Errorf("Error converting Token to bytes: %s", err.Error())
+	}
+	err = ctx.GetStub().PutState(tokenKey, tokenBytes)
+	if err != nil {
+		return fmt.Errorf("Error writing Token bytes to state: %s", err.Error())
+	}
+	return nil
+}
+
+// DecreaseAllowance allows a sender to decrease the funds of a delegate
+func (s *SmartContract) DecreaseAllowance(ctx contractapi.TransactionContextInterface, delegate cid.ClientIdentity, numTokens int) error {
+	token, err := s.getToken(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	if _, ok := token.Allowances[sender][delegate]; !ok {
+		return fmt.Errorf("Allowance for specified delegate from sender has not been created")
+	}
+	if token.Allowances[sender][delegate]-numTokens < 0 {
+		return fmt.Errorf("Requested tokens for transfer exceeds the existing allowance")
+	}
+
+	token.Balances[sender] = token.Balances[sender] + numTokens
+	token.Allowances[sender][delegate] = token.Allowances[sender][delegate] - numTokens
+	tokenBytes, err := json.Marshal(token)
+	if err != nil {
+		return fmt.Errorf("Error converting Token to bytes: %s", err.Error())
+	}
+	err = ctx.GetStub().PutState(tokenKey, tokenBytes)
+	if err != nil {
+		return fmt.Errorf("Error writing Token bytes to state: %s", err.Error())
+	}
+	return nil
+}
+
+// TransferFrom allows a delegate to spend the given allowance from an owner to a desired receiver
+func (s *SmartContract) TransferFrom(ctx contractapi.TransactionContextInterface, owner cid.ClientIdentity, buyer cid.ClientIdentity, numTokens int) error {
+	token, err := s.getToken(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	if _, ok := token.Allowances[owner][sender]; ok {
+		if numTokens > token.Allowances[owner][sender] {
+			return fmt.Errorf("Requested tokens for transfer exceeds the existing allowance")
+		}
+	} else {
+		return fmt.Errorf("Allowance for specified delegate from sender has not been created")
+	}
+	token.Allowances[owner][sender] = token.Allowances[owner][sender] - numTokens
+	token.Balances[buyer] = token.Balances[buyer] + numTokens
+	tokenBytes, err := json.Marshal(token)
+	if err != nil {
+		return fmt.Errorf("Error converting Token to bytes: %s", err.Error())
+	}
+	err = ctx.GetStub().PutState(tokenKey, tokenBytes)
+	if err != nil {
+		return fmt.Errorf("Error writing Token bytes to state: %s", err.Error())
 	}
 	return nil
 }
@@ -49,11 +200,11 @@ func main() {
 	chaincode, err := contractapi.NewChaincode(new(SmartContract))
 
 	if err != nil {
-		fmt.Printf("Error creating token chaincode: %s", err.Error())
+		fmt.Printf("Error creating Token chaincode: %s", err.Error())
 		return
 	}
 
 	if err := chaincode.Start(); err != nil {
-		fmt.Printf("Error starting token chaincode: %s", err.Error())
+		fmt.Printf("Error starting Token chaincode: %s", err.Error())
 	}
 }
