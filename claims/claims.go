2150744a1076a2b403abc038969fe6d973d2f3f8
John Yang <john.yang20@berkeley.edu>
2020-08-28 10:55:33 -0700

    [Claims] contract finished

--- claims/claims.go
+++ claims/claims.go
@@ -8,21 +8,31 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-const registryKey = "CLAIMS-KEY"
+const claimsKey = "CLAIMS-KEY"
+
+// Registry type for storing claims
+type Registry = map[cid.ClientIdentity](map[cid.ClientIdentity](map[string]string))
 
 // SmartContract provides functions for managing exchange
 type SmartContract struct {
 	contractapi.Contract // TODO, type aliasing?
 }
 
+// Claims struct for storing claim information
+type Claims struct {
+	registry Registry
+}
+
 // InitLedger opens the auction bidding process
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	registry := make(map[cid.ClientIdentity](map[cid.ClientIdentity](map[string]string)))
-	registryBytes, err := json.Marshal(registry)
+	claims := Claims{
+		registry: make(Registry),
+	}
+	claimBytes, err := json.Marshal(claims)
 	if err != nil {
 		return fmt.Errorf("Error converting registry to bytes: %s", err.Error())
 	}
-	err = ctx.GetStub().PutState(registryKey, registryBytes)
+	err = ctx.GetStub().PutState(claimsKey, claimBytes)
 	if err != nil {
 		return fmt.Errorf("Error writing registry bytes to state: %s", err.Error())
 	}
@@ -30,17 +40,84 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 // SetClaim allows a user to create a new claim or modify an existing claim
-func (s *SmartContract) SetClaim() error {
+func (s *SmartContract) SetClaim(ctx contractapi.TransactionContextInterface, subject cid.ClientIdentity, key string, value string) error {
+	claims, err := s.getClaims(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	claims.registry[sender][subject][key] = value
+	err = s.putClaims(ctx, *claims)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // SetSelfClaim allows a user to create a new claim or modify an existing claim on oneself
-func (s *SmartContract) SetSelfClaim() error {
+func (s *SmartContract) SetSelfClaim(ctx contractapi.TransactionContextInterface, key string, value string) error {
+	claims, err := s.getClaims(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	claims.registry[sender][sender][key] = value
+	err = s.putClaims(ctx, *claims)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-// RemoveClaim allows a user to remove an existing claim
-func (s *SmartContract) RemoveClaim() error {
+// RemoveClaim allows a user to remove an existing claim that it has issued or been issued
+func (s *SmartContract) RemoveClaim(ctx contractapi.TransactionContextInterface, issuer cid.ClientIdentity, subject cid.ClientIdentity, key string) error {
+	claims, err := s.getClaims(ctx)
+	if err != nil {
+		return err
+	}
+	sender := ctx.GetClientIdentity()
+	if sender != issuer && sender != subject {
+		return fmt.Errorf("Error, sender does not have authority to alter this claim")
+	}
+	if _, ok := claims.registry[issuer][subject][key]; !ok {
+		return fmt.Errorf("Error, specified claim does not exist")
+	}
+	delete(claims.registry[issuer][subject], key)
+	err = s.putClaims(ctx, *claims)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SmartContract) getClaims(ctx contractapi.TransactionContextInterface) (*Claims, error) {
+	claimBytes, err := ctx.GetStub().GetState(claimsKey)
+	if err != nil {
+		return nil, fmt.Errorf("Error retrieving Registry from state: %s", err)
+	}
+	if claimBytes == nil {
+		return nil, fmt.Errorf("Error, Registry has not been created")
+	}
+
+	// Unmarshal game data bytes into game data struct
+	claims := new(Claims)
+	err = json.Unmarshal(claimBytes, claims)
+	if err != nil {
+		return nil, fmt.Errorf("Error unmarshaling Registry bytes: %s", err)
+	}
+
+	return claims, nil
+}
+
+func (s *SmartContract) putClaims(ctx contractapi.TransactionContextInterface, claims Claims) error {
+	claimBytes, err := json.Marshal(claims)
+	if err != nil {
+		return fmt.Errorf("Error converting Registry to bytes: %s", err.Error())
+	}
+	err = ctx.GetStub().PutState(claimsKey, claimBytes)
+	if err != nil {
+		return fmt.Errorf("Error writing Registry bytes to state: %s", err.Error())
+	}
 	return nil
 }
 
