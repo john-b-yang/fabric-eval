2a27e22ea7d3fb15fbbfd7a0d37d3234eec97275
John Yang <john.yang20@berkeley.edu>
2020-08-28 17:52:26 -0700

    [RPS] Used variables instead of map

--- rps/rps.go
+++ rps/rps.go
@@ -16,6 +16,12 @@ import (
 type Game struct {
 	GameID  string                        `json:"gameid"`
 	Players map[cid.ClientIdentity]string `json:"players"`
+	P1      cid.ClientIdentity            `json:"p1"`
+	P2      cid.ClientIdentity            `json:"p2"`
+	P1Play  string                        `json:"p1play"`
+	P2Play  string                        `json:"p2play"`
+	P1Flag  bool                          `json:"p1flag"`
+	P2Flag  bool                          `json:"p2flag"`
 	Status  GameStatus                    `json:"status"`
 	Winner  cid.ClientIdentity            `json:"winner"`
 }
@@ -59,6 +65,7 @@ func (s *SmartContract) getGame(ctx contractapi.TransactionContextInterface, gam
 	return game, nil
 }
 
+// putGame is a helper function for committing RPS game data to transaction history
 func (s *SmartContract) putGame(ctx contractapi.TransactionContextInterface, game Game) error {
 	gameBytes, err := json.Marshal(game)
 	if err != nil {
@@ -76,6 +83,10 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 	newGame := Game{
 		GameID: uuid.New().String(),
 		Status: Open,
+		P1Play: UNSET,
+		P2Play: UNSET,
+		P1Flag: false,
+		P2Flag: false,
 	}
 	err := s.putGame(ctx, newGame)
 	if err != nil {
@@ -94,10 +105,17 @@ func (s *SmartContract) JoinGame(ctx contractapi.TransactionContextInterface, ga
 		return fmt.Errorf("Error, game has already started with 2 players")
 	}
 
-	game.Players[ctx.GetClientIdentity()] = UNSET
+	if game.P1 == nil {
+		game.P1 = ctx.GetClientIdentity()
+		game.P1Flag = true
+	} else if game.P2 == nil {
+		game.P2 = ctx.GetClientIdentity()
+		game.P2Flag = true
+	}
 
-	if len(game.Players) >= 3 {
+	if game.P1Flag && game.P2Flag {
 		game.Status = ChoosePlay
+		game.P1Flag, game.P2Flag = false, false
 	}
 
 	err = s.putGame(ctx, *game)
@@ -108,10 +126,8 @@ func (s *SmartContract) JoinGame(ctx contractapi.TransactionContextInterface, ga
 }
 
 // MakeChoice allows players to submit a choice that is hashed so as not to be revealed to the opponent
-func (s *SmartContract) MakeChoice(ctx contractapi.TransactionContextInterface, gameID string, choice int, hash string) error {
-	if choice < 0 || choice > 2 {
-		return fmt.Errorf("Error, invalid choice: must be 0 (rock), 1 (paper), or 2 (scissors)")
-	}
+func (s *SmartContract) MakeChoice(ctx contractapi.TransactionContextInterface, gameID string, hash string) error {
+	sender := ctx.GetClientIdentity()
 	game, err := s.getGame(ctx, gameID)
 	if err != nil {
 		return err
@@ -119,23 +135,19 @@ func (s *SmartContract) MakeChoice(ctx contractapi.TransactionContextInterface, 
 	if game.Status != ChoosePlay {
 		return fmt.Errorf("Error, game is not currently in the make choice phase")
 	}
-	if _, ok := game.Players[ctx.GetClientIdentity()]; !ok {
+	if game.P1 != sender && game.P2 != sender {
 		return fmt.Errorf("Error, player is not registered in this game")
 	}
 
-	hasher := sha256.New()
-	hasher.Write([]byte(strconv.Itoa(choice)))
-	hasher.Write([]byte(hash))
-	game.Players[ctx.GetClientIdentity()] = hex.EncodeToString(hasher.Sum(nil))
-
-	var choicesSet = true
-	for _, val := range game.Players {
-		if val != UNSET {
-			choicesSet = false
-		}
+	if game.P1 == sender {
+		game.P1Play = hash
+	} else if game.P2 == sender {
+		game.P2Play = hash
 	}
-	if choicesSet {
+
+	if game.P1Flag && game.P2Flag {
 		game.Status = RevealPlay
+		game.P1Flag, game.P2Flag = false, false
 	}
 
 	err = s.putGame(ctx, *game)
@@ -146,7 +158,8 @@ func (s *SmartContract) MakeChoice(ctx contractapi.TransactionContextInterface, 
 }
 
 // RevealChoice allows players to reveal the players' choices to determine the winner
-func (s *SmartContract) RevealChoice(ctx contractapi.TransactionContextInterface, gameID string, choice int, hash string) error {
+func (s *SmartContract) RevealChoice(ctx contractapi.TransactionContextInterface, gameID string, choice int, nonce string) error {
+	sender := ctx.GetClientIdentity()
 	game, err := s.getGame(ctx, gameID)
 	if err != nil {
 		return err
@@ -160,22 +173,21 @@ func (s *SmartContract) RevealChoice(ctx contractapi.TransactionContextInterface
 
 	hasher := sha256.New()
 	hasher.Write([]byte(strconv.Itoa(choice)))
-	hasher.Write([]byte(hash))
-	if hex.EncodeToString(hasher.Sum(nil)) == game.Players[ctx.GetClientIdentity()] {
-		game.Players[ctx.GetClientIdentity()] = strconv.Itoa(choice)
+	hasher.Write([]byte(nonce))
+
+	if sender == game.P1 && hex.EncodeToString(hasher.Sum(nil)) == game.P1Play {
+		game.P1Play = strconv.Itoa(choice)
+		game.P1Flag = true
+	} else if sender == game.P2 && hex.EncodeToString(hasher.Sum(nil)) == game.P2Play {
+		game.P2Play = strconv.Itoa(choice)
+		game.P2Flag = true
 	} else {
 		return fmt.Errorf("Error, choice + hash did not match the original player input")
 	}
 
-	var choicesRevealed = true
-	for _, val := range game.Players {
-		i, err := strconv.Atoi(val)
-		if err != nil || (err == nil && (i > 2 || i < 0)) {
-			choicesRevealed = false
-		}
-	}
-	if choicesRevealed {
+	if game.P1Flag && game.P2Flag {
 		game.Status = GameOver
+		game.P1Flag, game.P2Flag = false, false
 	}
 
 	err = s.putGame(ctx, *game)
@@ -195,26 +207,25 @@ func (s *SmartContract) DetermineWinner(ctx contractapi.TransactionContextInterf
 		return nil, fmt.Errorf("Error, game is not currently in the game over phase")
 	}
 
-	var p1, p2 cid.ClientIdentity
-	var p1play, p2play, index int
-	for key, val := range game.Players {
-		if index == 0 {
-			p1 = key
-			p1play, _ = strconv.Atoi(val)
-		} else if index == 1 {
-			p2 = key
-			p2play, _ = strconv.Atoi(val)
-		}
-		index++
-	}
+	p1play, p1err := strconv.Atoi(game.P1Play)
+	p2play, p2err := strconv.Atoi(game.P2Play)
+	p1valid := (p1err != nil) && (p1play >= 0) && (p1play <= 2)
+	p2valid := (p2err != nil) && (p2play >= 0) && (p2play <= 2)
 
-	var result = (3 + p1play - p2play)
-	if result == 1 || result == 4 {
-		game.Winner = p1
-	} else if result == 2 || result == 5 {
-		game.Winner = p2
-	} else {
+	if !p1valid && !p2valid {
 		game.Winner = nil
+	} else if p1valid && !p2valid {
+		game.Winner = game.P1
+	} else if !p1valid && p2valid {
+		game.Winner = game.P2
+	} else {
+		if p1play == p2play {
+			game.Winner = nil
+		} else if (p1play == 0 && p2play == 1) || (p1play == 1 && p2play == 2) || (p1play == 2 && p2play == 0) {
+			game.Winner = game.P1
+		} else {
+			game.Winner = game.P2
+		}
 	}
 
 	err = s.putGame(ctx, *game)
